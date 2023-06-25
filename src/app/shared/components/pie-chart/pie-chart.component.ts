a9a0bf341504ad93ebc1b56bea0c435ef90a6df5
angelos <angelos@echofin.com>
2023-06-25 19:55:32 +0300

    mock pie chart

--- src/app/shared/components/pie-chart/pie-chart.component.ts
+++ src/app/shared/components/pie-chart/pie-chart.component.ts
@@ -1,5 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import * as Highcharts from 'highcharts';
+import HighchartsExporting from 'highcharts/modules/exporting';
+import HighchartsExportData from 'highcharts/modules/export-data';
 
+HighchartsExporting(Highcharts);
+HighchartsExportData(Highcharts);
 @Component({
   selector: 'app-pie-chart',
   templateUrl: './pie-chart.component.html',
@@ -7,5 +12,32 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PieChartComponent {
-
+  ngOnInit() {
+    Highcharts.chart('chart-container', chartOptions);
+  }
 }
+
+const chartOptions: any = {
+  chart: {
+    type: 'pie',
+  },
+  title: {
+    text: 'Pie Chart',
+  },
+  series: [
+    {
+      name: 'Brands',
+      colorByPoint: true,
+      data: [
+        {
+          name: 'Chrome',
+          y: 61.41,
+        },
+        {
+          name: 'Internet Explorer',
+          y: 11.84,
+        },
+      ]
+    }
+  ],
+};
